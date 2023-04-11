0b968edaad6d96e94d719355b73fab187bc9ecf6
kkang3617 <rkdgus3617@naver.com>
2023-04-11 19:23:19 +0900

    DAY37 04.11.

---
+++ 2. web-api/10. 종합/css-mini-project2-kim/app.js/app.js
@@ -0,0 +1,12 @@
+
+function finish() {
+    const $monster = [...document.querySelectorAll('.monster')];
+    if(poX === endX) {  /* endX 절대값 */
+                 
+    } else if (death === 10) {
+
+    }
+
+
+
+}
--- 2. web-api/10. 종합/todolist/app.js
+++ 2. web-api/10. 종합/todolist/app.js
@@ -1,6 +1,23 @@
-
+/* 할일 추가 */
 const $input = document.querySelector('.input');
+const $button = document.querySelector('.icon');
 
-$input.addEventListener('click', e =>{
-    
+$button.addEventListener('click', e => {
+    if ($input.value === '') {
+        alert("할 일을 입력하세요");
+        return;
+    }
+    /* 할일 li추가 */
+    const $todoList = document.querySelector('.todoList');
+
+    const $checkbox = document.createElement('input');
+    $checkbox.type = 'checkbox';
+    $todoList.appendChild($checkbox);
+
+    const $newLi = document.createElement('li');
+    $newLi.textContent = $input.value;
+    $checkbox.appendChild($newLi);
+    $input.value = '';
+
+
 });
